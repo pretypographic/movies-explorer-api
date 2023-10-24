dc35312a10a08b401eb23dcda9477feefd0c910c
RO pretypographic <a.pesvi@yandex.ru>
2023-10-24 15:17:59 +0300

    4o6a8utb no44epgky okpyge2u9

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ const express = require('express');
 const cors = require('cors');
 const cookieParser = require('cookie-parser');
 const helmet = require('helmet');
+require('dotenv').config();
 
 // const path = require('path');
 const mongoose = require('mongoose');
