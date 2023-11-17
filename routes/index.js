28c2c9bdc4c33ff457e2d735b70cdca59ed33c38
RO pretypographic <a.pesvi@yandex.ru>
2023-11-17 18:25:17 +0400

    qukc

--- routes/index.js
+++ routes/index.js
@@ -4,11 +4,6 @@ const usersRouter = require('./users');
 const moviesRouter = require('./movies');
 const NotFoundError = require('../errors/not-found-err');
 
-// const { OK } = require('../utils/constants');
-
-// router.get('/', (req, res) => {
-//   res.status(OK).send('connected');
-// });
 router.use(usersRouter);
 router.use(moviesRouter);
 router.use('*', (req, res, next) => next(new NotFoundError('incorrect address')));
